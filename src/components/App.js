ee79ed18a9859c356a6b5eedb8f55afc1ed7ebeb
Brooke Kullberg <brookekullberg@gmail.com>
2021-07-07 20:33:39 -0700

    fix current build errors per #1

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,28 @@ import { useEffect, useState } from "react";
 function App() {
   const [audioInputs, setAudioInputs] = useState([]);
   const [audioOutputs, setAudioOutputs] = useState([]);
-  const [videoInputs, setVideoInputs] = useState([]);
+  // const [videoInputs, setVideoInputs] = useState([]);
   const [error, setError] = useState(null);
+  const [firstLoad, setFirstLoad] = useState(true);
+  // const [sinkIdSuccess, setSinkIdSuccess] = useState(false);
+
+  // const getDevices = () => {
+  //   navigator.mediaDevices.enumerateDevices().then(devices => {
+  //     console.log(audioOutputs)
+  //     populateOptions(devices);
+  //   }).catch(e => 
+  //     setError(e)
+  //   )
+  // }
 
   useEffect(() => {
+    const getDevices = () => {
+      navigator.mediaDevices.enumerateDevices().then(devices => {
+        populateOptions(devices);
+      }).catch(e => 
+        setError(e)
+      )
+    }
     getDevices();
     navigator.mediaDevices.addEventListener('devicechange', event => {
       getDevices();
@@ -14,36 +32,82 @@ function App() {
     }, []
   );
 
-  function getDevices() {
-    navigator.mediaDevices.enumerateDevices().then(devices => {
-      devices.forEach(e => console.log(e));
-      populateOptions(devices);
-    }).catch(e => 
-      setError(e)
-    )
-  }
+
+  // function attachSinkId(element, sinkId) {
+  //   // if (typeof element.sinkId !== 'undefined') {
+  //     element.setSinkId(sinkId)
+  //     .then(() => {
+  //       console.log(`Success, audio output device attached: ${sinkId}`);
+  //       setSinkIdSuccess(true);
+  //     })
+  //     .catch(error => {
+  //       console.log("ERROR!", error)
+  //     })
+  //   // } else {
+  //   //   console.log("did not work")
+  //   // }
+  // }
 
   function populateOptions(devices) {
-    const audioInputs = [];
-    const audioOutputs = [];
-    const videoInputs = [];
+    const audioIs = [];
+    const audioOs = [];
+    const videoIs = [];
     devices.forEach(e => {
       console.log(e)
       if (e.kind === "audioinput") {
-        audioInputs.push(e)
+        audioIs.push(e)
       } else if (e.kind === "audiooutput") {
-        audioOutputs.push(e)
+        audioOs.push(e)
       } else {
-        videoInputs.push(e);
+        videoIs.push(e);
       }
     })
-    setVideoInputs(videoInputs);
-    setAudioInputs(audioInputs);
-    setAudioOutputs(audioOutputs);
+    // setVideoInputs(videoIs);
+    setAudioInputs(audioIs);
+    setAudioOutputs(audioOs);
+    console.log("here")
+  }
+
+  // function handleAttachingSinkId() {
+  //   // gets AudioInput
+  //   const constraints = { 
+  //     audio: { deviceId: audioInputs[0].deviceId },
+  //     video: { devideId: videoInputs[0].deviceId}
+  //   }
+  //   navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+  //     window.stream = stream;
+  //     const videoElement = document.querySelector('video');
+  //     videoElement.srcObject = stream;
+  //     console.log(videoElement)
+  //     attachSinkId(videoElement, audioOutputs[0].deviceId)
+  //   });
+  // }
+
+  function connectDevice(...args) {
+    // const constraints = {'video': false, 'audio': true};
+    navigator.mediaDevices.getUserMedia(args[0])
+      .then(stream => {
+        const audioElement = document.querySelector('audio');
+        console.log(audioElement)
+        audioElement.src = stream;
+      })
   }
 
   function chooseDevice(type) {
-    // to do
+    if (window.stream) {
+      console.log("stream", window.stream)
+      window.stream.getTracks().forEach(track => {
+        console.log(track)
+        track.stop();
+      });
+    }
+    const element = document.getElementById(type);
+    if (type === "audioinput") {
+      console.log(element.selectedOptions[0].value)
+      connectDevice({'video': false, 'audio': {'echoCancellation': true, "deviceId": element.selectedOptions[0].value}});
+    } else if (type === "audiooutput") {
+
+    }
   }
 
   if (error) {
@@ -56,6 +120,11 @@ function App() {
       </div>
     )
   } else if (audioInputs.length > 0 && audioOutputs.length > 0) {
+    if (firstLoad) { 
+      console.log("connectinng device for first time")
+      connectDevice({'video': false, 'audio': {'deviceId': audioInputs[2] }}) 
+      setFirstLoad(false)
+    }
     return (
       <div>
         <header>
@@ -75,10 +144,10 @@ function App() {
               )}
             </select>
           </form>
-          <video id="video" playsInline autoPlay></video>
+          <audio id="audio" controls></audio>
         </header>
       </div>
-    );
+    )
   } else {
     return (
       <div>
